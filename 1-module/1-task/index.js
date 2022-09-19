5bb123c5a9395890705670cc649d2da83b6cc8df
Alena Gainulina <moi.pokypochki@yandex.ru>
2022-09-19 23:48:19 +0300

    написала функцию factorial(n)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    for (let i = n - 1; i >= 1; i--) {
+      n *= i;
+    }
+    return n;
+  }
 }
+let m = factorial(6);
+console.log(m);
